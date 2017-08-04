93b6917edba8aa7e66275d072ea65409fdd75b81
Jeffrey Swofford <jeffrey@swofford.com>
2017-08-04 15:27:34 -0500

    Configure webpack to use babel-loader

--- build/webpack.base.config.js
+++ build/webpack.base.config.js
@@ -23,7 +23,7 @@ module.exports = {
       },
       {
         test: /\.js$/,
-        loader: 'buble-loader',
+        loader: 'babel-loader',
         exclude: /node_modules/,
         query: {
           objectAssign: 'Object.assign'
